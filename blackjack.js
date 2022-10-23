0e5c646cb58da424db4b87f9f559b20d0852ca15
John Hamlin <johnhamlin@gmail.com>
2022-10-23 16:39:50 -0400

    Add deal() & updateScore() methods on Player class updateScore() handles hi/low aces but it's kind of janky and may be more overly complicated. Created some contants and eliminated some hard coding

--- blackjack.js
+++ blackjack.js
@@ -25,14 +25,15 @@ const CARDS = {
   king: 10,
 };
 
+const NUMBER_OF_DECKS = 1;
+const NUMBER_OF_PLAYERS = 4;
+const BLACKJACK = 21;
+
 // A single deck with four of every card
 const BASE_DECK = Object.keys(CARDS).reduce((deck, card) => {
   deck.push([card, 4]);
   return deck;
 }, []);
-
-const NUMBER_OF_DECKS = 1;
-const NUMBER_OF_PLAYERS = 4;
 
 class Player {
   constructor(playerNum) {
@@ -40,8 +41,38 @@ class Player {
     this.hand = [];
     this.score = 0;
     this.bet = 0;
-    this.money;
+    this.money = 500;
+    this.stand = false;
     this.busted = false;
+  }
+  updateScore() {
+    let tempAceStack = [];
+
+    // reset score by looping through entire hand and calculate total score (allows me to account for aceHigh vs aceLow)
+    this.score = this.hand.reduce((score, card) => {
+      if (card === 'ace') {
+        tempAceStack.push(card);
+      } else score += CARDS[card];
+      return score;
+    }, 0);
+
+    // handle aces
+    while (tempAceStack.length > 0) {
+      // Assumption: Only one ace could ever be high without causing a bust
+      // if an 11 wouldn't cause a bust, count it as an 11 and continue
+      if (this.score + CARDS.ace.aceHigh <= BLACKJACK) {
+        this.score += CARDS.ace.aceHigh;
+        tempAceStack.shift();
+        continue;
+      }
+      // otherwise, count the ace as aceLow
+      this.score += CARDS.ace.aceLow;
+      tempAceStack.shift();
+    }
+
+    // check for bust
+    if (this.score > 21) {
+    }
   }
 }
 
@@ -58,12 +89,12 @@ function shuffleDeck(numberOfDecks) {
     card,
     count * numberOfDecks,
   ]);
-  let maxIndexPlusOne = 13; // input for Math.random multiplier
+  let ranks = Object.keys(CARDS).length; // input for Math.random multiplier
 
   // generate shuffled deck for output
   while (numberOfCards > 0) {
     // generate a random index from 0 to (maxIndexPlusOne - 1) -- confusing!
-    let index = Math.floor(Math.random() * maxIndexPlusOne);
+    let index = Math.floor(Math.random() * ranks);
     // destructure array of arrays for easier reference
     let [currentCard, currentCardCount] = cardsAvailable[index];
     // add random card to shuffledDeck and update counters
@@ -74,7 +105,7 @@ function shuffleDeck(numberOfDecks) {
     // If that was the last card available of the type, remove it from cardsAvailable
     if (currentCardCount === 0) {
       cardsAvailable.splice(index, 1);
-      maxIndexPlusOne--;
+      ranks--;
     }
   }
   // TODO: Add cut card
@@ -83,13 +114,37 @@ function shuffleDeck(numberOfDecks) {
 
 function createPlayers(numberOfPlayers) {
   const players = [];
-  for (let i = 0; i < numberOfPlayers; i++) {
-    players.push(new Player(i));
-  }
+  // create new players and add them to array
+  for (let i = 0; i < numberOfPlayers; i++) players.push(new Player(i));
+  // add a dealer at the end
   players.push(new Dealer());
   return players;
 }
 
+function deal(players, shoe) {
+  // burn card
+  shoe.shift();
+  // deal two cards to each player
+  for (let i = 0; i < 2; i++) {
+    players.forEach(player => {
+      // deal a card
+      const card = shoe.shift();
+      // push card to player's hand
+      player.hand.push(card);
+      // add card value to score
+      player.updateScore();
+    });
+  }
+}
+
 let shoe = shuffleDeck(NUMBER_OF_DECKS);
 const players = createPlayers(NUMBER_OF_PLAYERS);
-console.table(players);
+// console.log(players);
+deal(players, shoe);
+
+// // test for players with aces
+// const playersWithAces = players.filter(
+//   player => player.hand[0] === 'ace' || player.hand[1] === 'ace'
+// );
+// // console.log(CARDS['ace']['aceHigh']);
+// console.table(playersWithAces);
